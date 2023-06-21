b231552b728057544e9c19432431d359b1831f49
Dan Halbert <halbert@halwitz.org>
2023-06-21 16:17:36 -0400

    add adafruit_toy_hacker

---
+++ ports/espressif/boards/adafruit_toy_hacker/board.c
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Scott Shawcroft for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "shared-bindings/microcontroller/Pin.h"
+#include "components/driver/include/driver/gpio.h"
+#include "components/hal/include/hal/gpio_hal.h"
+#include "common-hal/microcontroller/Pin.h"
+
+void board_init(void) {
+    reset_board();
+}
+
+void reset_board(void) {
+    // Turn on I2C and audio amp power by default.
+
+    // set pin to input to find 'rest state'
+    gpio_set_direction(11, GPIO_MODE_DEF_INPUT);
+    // wait 1 millis for pull to activate
+    mp_hal_delay_ms(1);
+    // read rest state (off)
+    bool restlevel = gpio_get_level(11);
+    gpio_set_direction(11, GPIO_MODE_DEF_OUTPUT);
+    // flip it!
+    gpio_set_level(11, !restlevel);
+}
+
+// Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
